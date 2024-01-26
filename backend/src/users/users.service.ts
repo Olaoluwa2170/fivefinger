9534780a91876842826ba4d2f868afa89dc63060
Babalola Elisha <elitebabson@gmail.com>
2024-01-26 16:28:34 +0100

    Add bcryptjs for password hashing

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly databaseService: DatabaseService) {}
-  create(createUserDto: Prisma.UserCreateInput) {
+  async create(createUserDto: Prisma.UserCreateInput) {
+    const { password, ...others } = createUserDto;
+    const hashPassword = await bcrypt.hash(password, 10);
     return this.databaseService.user.create({
-      data: createUserDto,
+      data: {
+        password: hashPassword,
+        ...others,
+      },
     });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.databaseService.user.findMany();
   }
 
   findOne(id: number) {
