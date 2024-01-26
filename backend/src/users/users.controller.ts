94f503e85f8e64a1c721b9f4e8e67db9b924ccff
Babalola Elisha <elitebabson@gmail.com>
2024-01-26 16:36:22 +0100

    Fix parameter type in remove method

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -35,7 +35,7 @@ export class UsersController {
   // }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.usersService.remove(+id);
   }
 }
