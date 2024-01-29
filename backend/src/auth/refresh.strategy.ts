2aa4ec0077f6ef9cdbecfb5fc57bc2b0151faa03
Babalola Elisha <elitebabson@gmail.com>
2024-01-29 13:45:03 +0100

    changed schema

---
+++ backend/src/auth/refresh.strategy.ts
@@ -0,0 +1,35 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { DatabaseService } from 'src/database/database.service';
+import { jwtConstants } from './constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly databaseService: DatabaseService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.refreshTokenSecret,
+    });
+  }
+
+  async validate(payload) {
+    const { id } = payload;
+    const user = this.databaseService.user.findUnique({
+      where: { id: id },
+    });
+    if (!user) throw new UnauthorizedException('Login to use this endpoint');
+
+    return user;
+  }
+  private static extractJWT(req): string | null {
+    if (req.cookies && 'access_token' in req.cookies) {
+      return req.cookies.access_token;
+    }
+    return null;
+  }
+}
