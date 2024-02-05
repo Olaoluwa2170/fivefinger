b793783127f67bf0bfbe74d19d352c6f6f326e50
Babalola Elisha <elitebabson@gmail.com>
2024-02-05 11:48:45 +0100

    Add headers and enable withCredentials in axiosPrivate

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -6,4 +6,6 @@ export default axios.create({
 });
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
